0e02ea125a944ff3620e83a3cf8918ed248b387d
jehaddayman <jehaddayman@gmail.com>
2025-08-11 23:47:22 +0300

    Update Header.tsx

--- alx-project-0x05/components/layout/Header.tsx
+++ alx-project-0x05/components/layout/Header.tsx
@@ -10,7 +10,10 @@ const Header: React.FC = () => {
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+        <Link
+          href="/"
+          className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight"
+        >
           Splash App
         </Link>
 
